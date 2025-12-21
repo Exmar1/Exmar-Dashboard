caa0d22baac69c3ea469bc17de1d9afd552ad9ae
Exmar <polevdenis74@gmail.com>
2025-12-22 01:19:00 +0500

    Оптимизация кода

--- src/data/dataProducts.js
+++ src/data/dataProducts.js
@@ -1,10 +1,19 @@
+import product1 from '../assets/images/product-1.webp'
+import product2 from '../assets/images/product-2.webp'
+import product3 from '../assets/images/product-3.webp'
+import product4 from '../assets/images/product-4.webp'
+import product5 from '../assets/images/product-5.webp'
+import product6 from '../assets/images/product-6.webp'
+import product7 from '../assets/images/product-7.webp'
+import product8 from '../assets/images/product-8.webp'
+
 export const products = [
-	{id: 1, title: "Nike Air Force 1 NDESTRUKT", price: "$35.17", inSale: true, image: "/images/product-1.webp"},
-	{id: 2, title: "Nike Space Hippie 04", price: "$57.22", image: "/images/product-2.webp"},
-	{id: 3, title: "Nike Air Zoom Pegasus 3", price: "$64.78", oldPrice: "$84.78", inSale: true, image: "/images/product-3.webp"},
-	{id: 4, title: "Nike Blazer Low 77 Vintage", price: "$50.79", inNew: true, image: "/images/product-4.webp"},
-	{id: 5, title: "Nike ZoomX SuperReper", price: "$9.57", inSale: true, image: "/images/product-5.webp"},
-	{id: 6, title: "Zoom Freak 2", price: "$61.46", image: "/images/product-6.webp"},
-	{id: 7, title: "Nike Air Max Zephyr", price: "$96.73", image: "/images/product-7.webp"},
-	{id: 8, title: "Jordan Delta", price: "$63.04", inNew: true, image: "/images/product-8.webp"}
+	{id: 1, title: "Nike Air Force 1 NDESTRUKT", price: "$35.17", inSale: true, image: product1},
+	{id: 2, title: "Nike Space Hippie 04", price: "$57.22", image: product2},
+	{id: 3, title: "Nike Air Zoom Pegasus 3", price: "$64.78", oldPrice: "$84.78", inSale: true, image:  product3},
+	{id: 4, title: "Nike Blazer Low 77 Vintage", price: "$50.79", inNew: true, image:  product4},
+	{id: 5, title: "Nike ZoomX SuperReper", price: "$9.57", inSale: true, image:  product5},
+	{id: 6, title: "Zoom Freak 2", price: "$61.46", image:  product6},
+	{id: 7, title: "Nike Air Max Zephyr", price: "$96.73", image:  product7},
+	{id: 8, title: "Jordan Delta", price: "$63.04", inNew: true, image:  product8}
 ]
